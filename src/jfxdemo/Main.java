23ad905ff7e099abf61a6bd76dbd48f17ceec112
Janet Davis <davisj@whitman.edu>
2021-02-02 13:54:21 -0800

    Move button event handler to anonymous function

--- src/jfxdemo/Main.java
+++ src/jfxdemo/Main.java
@@ -20,38 +20,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-public class Main extends Application // VERSION 3
-{
-    @Override
-    public void start(Stage primaryStage) {
-        Button button = new Button();
-        button.setText("Click Me");
-        button.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>()
-        {
-            @Override
-            public void handle(ActionEvent event) {
-                button.setText("Leave me alone!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Demo");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
-
-/*
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -84,4 +52,3 @@ public class Main extends Application // VERSION 4
         launch(args);
     }
 }
-*/
